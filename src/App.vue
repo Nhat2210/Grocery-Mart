b102d9056e1e34951c7b0c38183e70b28abb1c10
Nhat2210 <101515915+Nhat2210@users.noreply.github.com>
2024-11-19 21:31:52 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hihi">
+    xin chao
+    <p class="hihi_hehe">hehe</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const feature = ref();
+</script>
+
+<style lang="scss" scoped>
+.hihi{
+  width: 100px;
+  font-size: 29px;
+  &_hehe{
+    font-size: 50px;
+    font-family: Gordita, sans-serif;
+    font-weight: 500;
+  }
+}
+</style>
