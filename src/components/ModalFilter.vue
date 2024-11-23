f516b0bf93547eeae233dc5af522478c57e3b836
Nhat2210 <101515915+Nhat2210@users.noreply.github.com>
2024-11-24 02:54:14 +0700

    filter products

--- src/components/ModalFilter.vue
+++ src/components/ModalFilter.vue
@@ -7,12 +7,6 @@
         <div class="filter__column">
             <h3 class="filter__column__heading"></h3>
             <div class="slider">
-                <n-space vertical>
-                    <n-slider v-model:value="value" range :step="1" />
-                    <n-space>
-                    <n-input-number v-model:value="value[0]" size="small" />
-                    <n-input-number v-model:value="value[1]" size="small" />
-                </n-space>
             </div>
         </div>
         <!--Filter column 2-->
@@ -23,7 +17,6 @@
 </template>
 
 <script>
-import { NSpace, NSlider } from 'naive-ui';
 </script>
 
 <style lang="scss" scoped>
--- src/components/TotalProduct.vue
+++ src/components/TotalProduct.vue
@@ -1,13 +1,37 @@
-<script>
-import { ModalFilter } from '../components/ModalFilter.vue';
-import { defineProps } from 'vue';
+<script setup>
+import { ref } from 'vue';
+import { NSpace, NSlider, NInputNumber, NSelect, NInputGroup, NButton, NInput } from 'naive-ui';
 
-const props = defineProps({
-    isshowModal: {
-        type: Boolean,
-        require: true,
+
+//show modal filter 
+const showModal = ref(false);
+const toggleModal = () => {
+    showModal.value = !showModal.value;
+}
+//close modal filter
+const closeModal = () => {
+    showModal.value = false;
+}
+
+//set value for slider
+const value = ref([0,100]);
+
+//set option select
+const choice = ref(null);
+const options = [
+    {
+        label: '500g',
+        value: '500g',
+    },
+    {
+        label: '1000g',
+        value: '1000g',
+    },
+    {
+        label: '1500g',
+        value: '1500g',
     }
-})
+];
 </script>
 
 <template>
@@ -15,11 +39,81 @@ const props = defineProps({
     <div class="Total-product">
         <div class="Total-product__title">
             <h2 class="Total-product__heading">Total LavAzza 1320</h2>
-            <button @click="isShowModal = true" class="filter-btn">
+            <button class="filter-btn" @click="toggleModal()">
                 Filter
                 <img src="../assets/icons/filter.svg" alt="" class="filter-btn__icon">
             </button>
-            <ModalFilter/>
+        </div>
+        
+        <!--Modal-->
+        <div class="filter" v-if="showModal">
+            <h3 class="filter__heading">Filter</h3>
+            <form action="" class="filter__form">
+                <div class="filter__row">
+                    <!--Filter column 1-->
+                    <div class="filter__column">
+                        <h3 class="filter__column__heading">Price</h3>
+                        <div class="slider">
+                            <n-space vertical>
+                                <div class="slider__bar">
+                                    <n-slider v-model:value="value" range :step="1" />
+                                </div>
+                                <n-space>
+                                    <div class="filter__minmax">
+                                        <div class="min-value">
+                                            <p class="min-value__title">Minimum</p>
+                                            <n-input-number v-model:value="value[0]" size="small" />
+                                        </div>
+                                        <div class="max-value">
+                                            <p class="max-value__title">Maximum</p>
+                                            <n-input-number v-model:value="value[1]" size="small" />
+                                        </div>
+                                    </div>
+                                </n-space>
+                            </n-space>
+                        </div>
+                    </div>
+                    <div class="filter__separate"></div>
+                    <!--Filter column 2-->
+                    <div class="filter__column">
+                        <h3 class="filter__column__heading">Size/Weight</h3>
+                        <div class="sizeweight">
+                            <div class="filter__column__select">
+                                <n-space vertical>
+                                    <n-select v-model:value="choice" :options="options" />
+                                </n-space>
+                            </div>
+                            <ul class="filter__column__list">
+                                <li class="filter__column__item">Small</li>
+                                <li class="filter__column__item">Medium</li>
+                                <li class="filter__column__item">Large</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="filter__separate"></div>
+                    <!--Filter column 3-->
+                    <div class="filter__column">
+                        <h3 class="filter__column__heading">Price</h3>
+                        <div class="filter__column__brand">
+                            <n-input-group>
+                                <n-input :style="{ width: '50%' }" />
+                                <n-button type="primary" ghost>
+                                    Search
+                                </n-button>
+                            </n-input-group>
+                        </div>
+                        <ul class="filter__column__list">
+                            <li class="filter__column__item">Lavazza</li>
+                            <li class="filter__column__item">Nescafe</li>
+                            <li class="filter__column__item">Starbucks</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="filter__button">
+                    <button class="filter__button__cancel">Cancel</button>
+                    <button class="filter__button__showResult">Show Result</button>
+                </div>
+            </form>
         </div>
         <div class="Total-product__list">
             <!--Product 1-->
@@ -199,6 +293,116 @@ const props = defineProps({
                     font-size: 1.5rem;
                     font-weight: 500;
                 }
+            }
+        }
+    }
+}
+
+
+
+// css filter
+.filter{
+    padding: 30px;
+    background: #fff;
+    border-radius: 20px;
+    position: relative;
+    z-index: 999;
+    &__heading{
+        font-weight: 500;
+        font-size: 2.2rem; 
+        margin-bottom: 30px;
+    }
+    &::before{
+        content: url('../assets/icons/dropdown-arrow.svg');
+        position: absolute;
+        top: -15px;
+        right: 30px;
+    }
+    &__row{
+        display: flex;
+        justify-content: space-between;
+    }
+    &__column{
+        width: 28%;
+        &__heading{
+            font-weight: 500;
+            font-size: 2.2rem;
+            margin-bottom: 20px;
+        }
+        &__select{
+            margin-bottom: 25px;
+        }
+        &__brand{
+            margin-bottom: 25px;
+        }
+        &__list{
+            display: flex;
+            justify-content: space-between;
+        }
+        &__item{
+            padding: 5px 10px;
+            background-color: #bab9cb;
+            border-radius: 3px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            cursor: pointer;
+            &:hover{
+                background: #dcdce2;
+            }
+        }
+    }
+    .slider__bar{
+        margin-bottom: 10px;
+    }
+    &__minmax{
+        display: flex;
+        gap: 25px;
+        .min-value{
+            font-weight: 500;
+            font-size: 1.4rem;
+            &__title{
+                margin-bottom: 10px;
+            }
+        }
+        .max-value{
+            font-weight: 500;
+            font-size: 1.4rem;
+            &__title{
+                margin-bottom: 10px;
+            }
+        }
+    }
+    &__separate{
+        width: 1px;
+        height: 150px;
+        background-color: #9E9DA8;
+        align-self: center;
+        opacity: 0.2;
+    }
+    &__button{
+        justify-self: end;     
+        &__cancel{
+            border: none;
+            background-color: #ededf5;
+            padding: 8px 8px;
+            border-radius: 5px;
+            cursor: pointer;
+            &:hover{
+                background-color: rgb(230, 82, 82);
+                color: #fff;
+            }
+        }
+        &__showResult{
+            border:none;
+            background-color: #ffd468;
+            padding: 9px 10px;
+            border-radius: 5px;
+            margin-left: 15px;
+            cursor: pointer;
+            &:hover{
+                background-color: #FFB700;
+                color: #fff;
             }
         }
     }
