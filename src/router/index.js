93b421d5194fb86c306e293f7e40eb3d653efee4
Nhat2210 <101515915+Nhat2210@users.noreply.github.com>
2024-11-25 06:46:59 +0700

    add login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 import App from "@/App.vue";
 import Signup from "@/views/Signup.vue";
+import Login from "@/views/Login.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{ path: "/", component: Signup }],
+  routes: [
+    {
+      path: "/Login",
+      component: Login,
+    },
+    {
+      path: "/SignUp",
+      component: Signup,
+    },
+  ],
 });
 
 export default router;
